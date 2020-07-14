3363b7a1a4f9b806d90d4f78264345938cb9fdf0
zina31 <mostafa@white-code.co.uk>
2020-07-14 09:32:28 +0200

    care one changes

---
+++ sale_subscriptions_enhanchment/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Sales Subscription Enhancment',
+    'version': '13.0.1.0.0',
+    'author': 'White-code (Zienab Abd El Nasser)',
+    'category': 'Sale',
+    'depends': [
+        'sale_subscription',
+    ],
+    'data': [
+        'views/sale_subscription_template.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
