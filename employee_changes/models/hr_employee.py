f2c2b9d6bbc9320e8c4d7934faa1dd69625548fc
zina31 <mostafa@white-code.co.uk>
2020-08-16 15:26:04 +0200

    end of service

--- employee_changes/models/hr_employee.py
+++ employee_changes/models/hr_employee.py
@@ -1,5 +1,7 @@
-from odoo import models, fields
+from odoo import models, fields ,api
 from dateutil import relativedelta
+from datetime import datetime
+
 
 
 class HrEmployee(models.Model):
@@ -42,3 +44,31 @@ class HrEmployee(models.Model):
                         'email_to': '%s, %s, %s' % (employee.work_email,employee.coach_id.work_email or False,employee.parent_id.work_email or False),
                     }
                     visa_expiration_template.send_mail(employee.id, force_send=True, email_values=email_values)
+
+    # end of service section
+
+    employee_type2 = fields.Selection([('worker','Worker'),('employee','Employee')],string='End of Service For')
+    calculate_balance = fields.Boolean('')
+    end_service_date = fields.Date('Date')
+    balance = fields.Float('Balance' ,compute="_get_end_service_balance")
+    
+    @api.depends('employee_type2','calculate_balance','end_service_date')
+    def _get_end_service_balance(self):
+        balance = 0
+        if self.calculate_balance:
+            if not self.end_service_date :
+                self.calculate_balance = 0
+                raise UserError("You need to put End Date First")
+            contract = self.env['hr.contract'].search([('employee_id','=',self._origin.id),('state','=','open')],limit=1)
+            start = datetime.strptime(str(contract.date_start), '%Y-%m-%d')
+            end = datetime.strptime(str(self.end_service_date), '%Y-%m-%d')
+            r = relativedelta.relativedelta(end,start)
+            years = int(r.years)
+            if self.employee_type2 == 'worker':
+                if years >= 2:
+                    balance = contract.wage * years
+                else :
+                    balance = contract.wage * 0.5 * years
+            if self.employee_type2 == 'employee':
+                balance = contract.wage * years
+        self.balance = balance
