7f8d8af9f92efd2cf08a115a34ad2add80539f86
raghupathy <raghupathy@emxcelsolutions.com>
2020-10-25 16:46:30 +0530

    added new masters for vehicle color and vehicle model

--- vehicle_part/__manifest__.py
+++ vehicle_part/__manifest__.py
@@ -3,13 +3,13 @@
     'version': '13.0.1.0.0',
     'author': 'White-code, Kalpesh Gajera',
     'category': 'Sale',
-    'depends': [
-        'sale',
+    'depends': [ 'sale','fleet'
     ],
     'data': [
         'security/ir.model.access.csv',
         'views/vehicle_part_view.xml',
         'views/sale_view.xml',
+        'views/fleet_master_views.xml',
     ],
     'installable': True,
     'auto_install': False,
---
+++ vehicle_part/models/fleet_master.py
@@ -0,0 +1,22 @@
+from odoo import models, fields
+
+class FleetVehicle(models.Model):
+	_inherit = 'fleet.vehicle'
+	_description = 'Vehicle'
+	
+	color = fields.Many2one('fleet.color',string='color')
+	model_year = fields.Many2one('fleet.model',string='Model Year', help='Year of the model')
+
+
+class FleetColor(models.Model):
+	_name = 'fleet.color'
+	_description = 'Vehicle Color'
+
+	name = fields.Char('Color')
+
+class FleetModel(models.Model):
+	_name = 'fleet.model'
+	_description = 'Vehicle Model'
+
+	name = fields.Char('Model')
+
