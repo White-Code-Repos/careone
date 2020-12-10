e4f53b81369ff819b00397517e85d6dbdae3c7e9
anabil <anabil@white-code.co.uk>
2020-12-10 19:26:18 +0200

    Update models.py

--- careone_mrp/models/models.py
+++ careone_mrp/models/models.py
@@ -50,8 +50,9 @@ class MrpProduction(models.Model):
     @api.constrains('sale_order_id','mrp_group_id')
     def set_mrp_users(self):
         self.user_ids = self.sale_order_id.user_ids or self.mrp_group_id.user_ids
-        mrp_grp_id = self.sale_order_id.mrp_group_id or self.mrp_group_id
-        self.location_src_id = mrp_grp_id.location_id
+        if sale_order_id :
+            mrp_grp_id = self.sale_order_id.mrp_group_id or self.mrp_group_id
+            self.location_src_id = mrp_grp_id.location_id
         # self.location_dest_id = mrp_grp_id.location_id
 
     @api.model
