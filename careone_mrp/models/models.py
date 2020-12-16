ee2ca4f0554fcb489926ddb12c25ca13fcad3b04
anabil <anabil@white-code.co.uk>
2020-12-17 01:07:38 +0200

    Update models.py

--- careone_mrp/models/models.py
+++ careone_mrp/models/models.py
@@ -1,6 +1,6 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
-
+from odoo.exceptions import UserError, RedirectWarning
 
 class MrpGroup(models.Model):
     _name = 'mrp.group'
