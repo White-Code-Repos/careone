8fdbfdbd2ce88fd48135d8a6c6196e9aac34ed32
anabil <anabil@white-code.co.uk>
2020-12-21 22:42:52 +0200

    Update models.py

--- careone_mrp/models/models.py
+++ careone_mrp/models/models.py
@@ -112,7 +112,7 @@ class MrpWorkorder(models.Model):
         is_lastorder = self.env['mrp.workorder'].search([('state','!=','done'),('production_id','=',self.production_id.id),('id','!=',self.id)])
         if not is_lastorder:
             #self.production_id.write({'state':'to_close'})
-            #self.qty_producing = self.qty_production
+            self.qty_producing = self.qty_production
             #self.production_id.post_inventory()
             #self.production_id.button_mark_done()
             self.do_finish()
