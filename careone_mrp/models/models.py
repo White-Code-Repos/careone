fc27adc824d44a859c7a9d54a2eaaab0200e2899
anabil <anabil@white-code.co.uk>
2020-12-22 21:28:49 +0200

    Update models.py

--- careone_mrp/models/models.py
+++ careone_mrp/models/models.py
@@ -75,9 +75,15 @@ class MrpProduction(models.Model):
             
         self.mrp_group_id = mrp_grp_id
         self.location_src_id = mrp_grp_id.location_id
+        picktype=self.env['stock.picking.type'].search([('default_location_src_id','=',self.location_src_id.id),('code','=','mrp_operation')],limit=1)
+        
         self.picking_type_id = self.env['stock.picking.type'].search([('default_location_src_id','=',self.location_src_id.id),('code','=','mrp_operation')],limit=1)
-        #self.write({'picking_type_id':self.env['stock.picking.type'].search([('default_location_src_id','=',self.location_src_id.id),('code','=','mrp_operation')],limit=1),
-        #'location_src_id':mrp_grp_id.location_id})
+        moves = self.env['stock.move.line').search([('mrp_id','=',self.id),('location_dest_id','!=',self.location_dest_id)])
+        for move in moves:
+            move.write({'location_id':mrp_grp_id.location_id})
+        #self.write({'picking_type_id':
+        #self.env['stock.picking.type'].search([('default_location_src_id','=',self.location_src_id.id),('code','=','mrp_operation')],limit=1)
+        #,'location_src_id':mrp_grp_id.location_id})
         # self.location_dest_id = mrp_grp_id.location_id
 
     @api.model
