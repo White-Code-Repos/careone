d087cf37e78c526fae696636ebf69ddf0e273db7
anabil <anabil@white-code.co.uk>
2020-12-22 21:39:30 +0200

    Update models.py

--- careone_mrp/models/models.py
+++ careone_mrp/models/models.py
@@ -78,7 +78,7 @@ class MrpProduction(models.Model):
         picktype=self.env['stock.picking.type'].search([('default_location_src_id','=',self.location_src_id.id),('code','=','mrp_operation')],limit=1)
 
         self.picking_type_id = self.env['stock.picking.type'].search([('default_location_src_id','=',self.location_src_id.id),('code','=','mrp_operation')],limit=1)
-        moves = self.env['stock.move.line'].search([('mrp_id','=',self.id),('location_dest_id','!=',self.location_dest_id)])
+        moves = self.env['stock.move.line'].search([('production_id','=',self.id),('location_dest_id','!=',self.location_dest_id)])
         for move in moves:
             move.write({'location_id':mrp_grp_id.location_id})
         #self.write({'picking_type_id':
