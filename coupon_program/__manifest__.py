2ad1f96ca2213add32f05b2bb44caa925abf7901
Abdel-Rahman-Warda <awarda@white-code.co.uk>
2020-12-23 21:42:54 +0200

    Update __manifest__.py

--- coupon_program/__manifest__.py
+++ coupon_program/__manifest__.py
@@ -3,7 +3,7 @@
     'name': "Coupon Program",
     'category': 'Coupon',
     'version': '0.1',
-    'depends': ['base','sale_coupon'],
+    'depends': ['base','sale_coupon','fleet','sale'],
                 #,'sale_coupon','partner_fleet_sale_enhancement','sale_coupons_enhanchment','sale','fleet'],
     # always loaded
     'data': [
