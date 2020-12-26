0e24d0e2b34fbe312ae974156aec094e8e3e0738
Abdel-Rahman-Warda <awarda@white-code.co.uk>
2020-12-26 20:21:57 +0200

    Careone Access

---
+++ careone_access/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Careone Access",
+    'author': "White-Code, Abdulrahman Warda",
+    'version': '0.1',
+    'description':'''
+    Configuring new access rights, rules and groups depending on requierments
+    ''',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+    ],
+}
