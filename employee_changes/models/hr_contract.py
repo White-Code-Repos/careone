de14aa154301f7359a8e3566b4ea3345a6186eb2
Abdel-Rahman-Warda <awarda@white-code.co.uk>
2020-12-26 14:52:27 +0200

    contract changes

--- employee_changes/models/hr_contract.py
+++ employee_changes/models/hr_contract.py
@@ -5,6 +5,16 @@ class HrContract(models.Model):
 
     attachment_ids = fields.One2many('contract.attachment','contract_id')
 
+    settelment_allowance = fields.Float()
+    transportation_allowance = fields.Float()
+    other_allowance = fields.Float()
+
+    gosi_record_number = fields.Char()
+    gosi_record_date = fields.Date()
+    gosi_record_type = fields.Selection([('saudi','Saudi'),('not_saudi','Not Saudi')])
+    gosi_record_salary = fields.Float()
+    gosi_record_end_date = fields.Date()
+
 class ContractAttch(models.Model):
     _name = 'contract.attachment'
 
