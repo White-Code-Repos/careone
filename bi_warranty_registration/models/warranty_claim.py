6f22f1c237ef8bf7bb1e851afcd9de3a8c8ee025
anabil <anabil@white-code.co.uk>
2020-12-26 18:36:12 +0200

    Update warranty_claim.py

--- bi_warranty_registration/models/warranty_claim.py
+++ bi_warranty_registration/models/warranty_claim.py
@@ -74,9 +74,9 @@ class crm_claim(models.Model):
 	warranty = fields.Many2one('product.warranty','Related Warranty')
 	stage_match = fields.Selection([('first','first'), ('second','second'), ('third','third'),('fourth','fourth')], 'Stage Match', default='first')
 	viechle_id = fields.Many2one('fleet.veichle')
-    size = fields.Char(string="Car Size")
-    model_id = fields.Many2one('fleet.model')
-    
+	size = fields.Char(string="Car Size")
+	model_id = fields.Many2one('fleet.model')
+	
 	@api.onchange('partner_id')
 	def customer_details(self):
 		self.partner_phone = self.partner_id.phone
