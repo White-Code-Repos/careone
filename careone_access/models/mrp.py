f5d36b28fec8474c635fe838514375c5a6afc7e2
Abdel-Rahman-Warda <awarda@white-code.co.uk>
2020-12-28 08:41:47 +0200

    mrp edit

--- careone_access/models/mrp.py
+++ careone_access/models/mrp.py
@@ -5,11 +5,11 @@ from odoo import api, fields, models, _
 class AccountMove(models.Model):
     _inherit = 'mrp.production'
 
-    current_group_mrp = fields.Boolean(compute="_compute_current_group_mrp")
-
-    def _compute_current_group_mrp(self):
-        flag = self.pool.get('res.users').has_group(self.env.user, 'careone_access.careone_worker')
-        if flag:
-            self.current_group_mrp = True
-        else:
-            self.current_group_mrp = False
+    # current_group_mrp = fields.Boolean(compute="_compute_current_group_mrp")
+    #
+    # def _compute_current_group_mrp(self):
+    #     flag = self.pool.get('res.users').has_group(self.env.user, 'careone_access.careone_worker')
+    #     if flag:
+    #         self.current_group_mrp = True
+    #     else:
+    #         self.current_group_mrp = False
