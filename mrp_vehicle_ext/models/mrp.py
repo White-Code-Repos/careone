ec71541e15999c38f66adb4b623b7bec13c3e56f
Abdel-Rahman-Warda <awarda@white-code.co.uk>
2020-12-29 23:31:24 +0200

    mrp kanban vehicle

--- mrp_vehicle_ext/models/mrp.py
+++ mrp_vehicle_ext/models/mrp.py
@@ -3,6 +3,12 @@ from odoo.exceptions import ValidationError
 
 
 class MRP_inherit_EXT(models.Model):
-    _inherit = 'mrp.production'
+    _inherit = 'mrp.workorder'
 
-    
+    vehicle_id = fields.Many2one('fleet.vehicle', string="Vehicle", compute="_compute_vehicle")
+    def _compute_vehicle(self):
+        if self.production_id:
+            if self.production_id.vehicle_id_sale:
+                self.vehicle_id = self.production_id.vehicle_id_sale.id
+        if not self.vehicle_id:
+            self.vehicle_id = False
