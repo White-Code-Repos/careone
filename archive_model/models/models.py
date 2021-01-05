7b3742b2dbd5f9f7e932af7fcadfa19baf2f9c0a
Mohab Ahmed <MohabAhmedHamed1999>
2021-01-05 06:02:52 +0000

    Test server

--- archive_model/models/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class ArchiveModel(models.Model):
-
-    _inherit = 'fleet.vehicle.model'
-
-    active = fields.Boolean(string="Active" , default=True)
