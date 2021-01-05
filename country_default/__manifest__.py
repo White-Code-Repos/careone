0bb466fb73752470c864cbb3cdfbc58bc9db2f99
Mohab Ahmed <MohabAhmedHamed1999>
2021-01-05 06:05:25 +0000

    Country Defualt

---
+++ country_default/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Country Default",
+
+    'summary': """  Set Saudia default country in contacts """,
+
+    'description': """   """,
+
+    'author': "Mohab Ahmed Hamed",
+
+    'website': "https://www.Algorithm.com",
+
+    'category': '',
+
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [ ],
+}
