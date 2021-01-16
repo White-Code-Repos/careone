f0145bfd8141292159ddec9b71d0080893959496
Mohab Ahmed <MohabAhmedHamed1999>
2021-01-16 04:35:33 +0000

    fix size exception

--- vehicle_no_search/models/models.py
+++ vehicle_no_search/models/models.py
@@ -6,10 +6,10 @@ class FleetVehicleNumberSearch(models.Model):
 
     _inherit = 'fleet.vehicle'
 
-    # @api.model
-    # def name_get(self):
-    #     res = []
-    #     for rec in self:
-    #         res.append((rec.id, '%s  -  %s' %(rec.model_id.name , rec.license_plate)))
-    #     return res
+    @api.model
+    def name_get(self):
+        res = []
+        for rec in self:
+            res.append(('%s  -  %s' %(rec.model_id.name , rec.license_plate)))
+        return res
 
