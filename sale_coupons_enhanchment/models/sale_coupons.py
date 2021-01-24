8bbea9df817ef83d2c58b9f451ebb46df9492a1c
MohabAhmedHamed1999 <mahmed@white-code.co.uk>
2021-01-24 12:43:35 +0000

    test

--- sale_coupons_enhanchment/models/sale_coupons.py
+++ sale_coupons_enhanchment/models/sale_coupons.py
@@ -42,12 +42,12 @@ class SaleCoupon(models.Model):
     def _compute_expiration_date(self):
         for this in self:
             this.expiration_date = 0
-            # for coupon in this.filtered(lambda x: x.program_id.validity_duration > 0):
-            #     # if not coupon.from_subscription or not coupon.expiration_date_2  :
-            #     #     coupon.expiration_date = (coupon.create_date + relativedelta(days=coupon.program_id.validity_duration)).date()
-            #     #     coupon.expiration_date_2 = (coupon.create_date + relativedelta(days=coupon.program_id.validity_duration)).date()
-            #     # else:
-            #     coupon.expiration_date = coupon.expiration_date_2
+            for coupon in this.filtered(lambda x: x.program_id.validity_duration > 0):
+                # if not coupon.from_subscription or not coupon.expiration_date_2  :
+                #     coupon.expiration_date = (coupon.create_date + relativedelta(days=coupon.program_id.validity_duration)).date()
+                #     coupon.expiration_date_2 = (coupon.create_date + relativedelta(days=coupon.program_id.validity_duration)).date()
+                # else:
+                coupon.expiration_date = coupon.expiration_date_2
 
 
 class SaleCouponProgram(models.Model):
