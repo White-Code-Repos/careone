fde63a5856d167d1897c50d097855ff383ac043c
Abdel-Rahman-Warda <75036891+Abdel-Rahman-Warda@users.noreply.github.com>
2021-01-25 00:53:10 +0200

    update_subscriper_state

--- sale_subscriptions_enhanchment/models/sale_subscription.py
+++ sale_subscriptions_enhanchment/models/sale_subscription.py
@@ -15,9 +15,9 @@ _logger = logging.getLogger(__name__)
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    is_subscriper = fields.Boolean(compute="_compute_subscripe_state")
-    def _compute_subscripe_state(self):
-        for this in self:
+    is_subscriper = fields.Boolean(default=Flase)
+    def compute_subscripe_state(self):
+        for this in self.env['res.partner'].search([]):
             if this.subscription_count > 0 :
                 this.is_subscriper = True
             else:
