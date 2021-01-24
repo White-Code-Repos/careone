b41c0d6bc73f3f7099b2ab23e9b5ecb9704d5413
Abdel-Rahman-Warda <75036891+Abdel-Rahman-Warda@users.noreply.github.com>
2021-01-25 01:22:29 +0200

    Update sale_subscription.py

--- sale_subscriptions_enhanchment/models/sale_subscription.py
+++ sale_subscriptions_enhanchment/models/sale_subscription.py
@@ -16,13 +16,13 @@ class Partner(models.Model):
     _inherit = 'res.partner'
 
     is_subscriper = fields.Boolean(compute="_compute_subscripe_state")
-    # subscriper = fields.Boolean(default=False)
+    subscriper = fields.Boolean(default=False)
     def compute_subscripe_state(self):
         for this in self.env['res.partner'].search([]):
             if this.subscription_count > 0 :
-                this.is_subscriper = True
+                this.subscriper = True
             else:
-                this.is_subscriper = False
+                this.subscriper = False
 class SalesSubscription(models.Model):
     _inherit = 'sale.subscription'
 
