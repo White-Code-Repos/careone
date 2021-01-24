691dca48e334a8f0486824e9b626b97c60f1495a
MohabAhmedHamed1999 <mahmed@white-code.co.uk>
2021-01-24 15:41:26 +0000

    test

--- sale_coupons_enhanchment/models/sale_coupons.py
+++ sale_coupons_enhanchment/models/sale_coupons.py
@@ -26,7 +26,7 @@ class SaleCoupon(models.Model):
             for coupon in this.filtered(lambda x: x.program_id.validity_duration > 0):
                 coupon.expiration_date = coupon.expiration_date_2
             for coupon in this.filtered(lambda y: y.program_id.validity_duration == 0):
-                if not coupon.create_date < datetime.now().date():
+                if not coupon.create_date.date() < datetime.now().date():
                     coupon.expiration_date = 0
 
 class SaleCouponProgram(models.Model):
