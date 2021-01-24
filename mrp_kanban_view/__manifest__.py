8cc41856565517062033284ab5df6d9e42ac6049
MohabAhmedHamed1999 <mahmed@white-code.co.uk>
2021-01-24 16:55:32 +0000

    test

---
+++ mrp_kanban_view/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale Order Line Fixed Discount",
+
+    'summary': """ """,
+
+    'description': """ """,
+
+    'author': "Mohab Ahmed Hamed",
+
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Uncategorized',
+
+    'version': '0.1',
+
+    'depends': ['base' , 'sale'],
+
+    'data': ['views/view.xml'],
+}
---
+++ sale_order_line_discount/models/model.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class SaleOrderLine(models.Model):
+
+    _inherit = 'sale.order.line'
+
+    fixed_discount = fields.Float(string="Fixed Discount")
+
+    # @api.onchange('fixed_discount')
+    # def action_calculate_discount(self):
+    #     if self.fixed_discount:
+    #         self.discount = (self.fixed_discount / self.price_subtotal) * 100
+    #
+    # @api.onchange('discount')
+    # def action_calculate_fixed_discount(self):
+    #     if self.discount:
+    #         self.fixed_discount = (self.fixed_discount / self.price_subtotal) * 100
