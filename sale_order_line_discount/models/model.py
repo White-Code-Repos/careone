4695870c974074c67694bb167aee36aa4ea9a026
MohabAhmedHamed1999 <mahmed@white-code.co.uk>
2021-01-24 21:00:46 +0000

    test

--- sale_order_line_discount/models/model.py
+++ sale_order_line_discount/models/model.py
@@ -8,10 +8,7 @@ class SaleOrderLine(models.Model):
 
     fixed_discount = fields.Float(string="Fixed Discount")
 
-    @api.depends('fixed_discount')
+    @api.onchange('fixed_discount')
     def action_calculate_discount(self):
-        self.discount = (self.fixed_discount / self.price_unit) * 100
-
-    @api.depends('discount')
-    def action_calculate_fixed_discount(self):
-        self.fixed_discount = (self.discount / 100) * self.price_unit
+        if self.fixed_discount:
+            self.discount = (self.fixed_discount / self.price_unit) * 100
