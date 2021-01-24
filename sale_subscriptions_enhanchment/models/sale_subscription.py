2e7e79d857bf1915225b9e64de682a6955e95139
Abdel-Rahman-Warda <75036891+Abdel-Rahman-Warda@users.noreply.github.com>
2021-01-24 23:14:39 +0200

    Update sale_subscription.py

--- sale_subscriptions_enhanchment/models/sale_subscription.py
+++ sale_subscriptions_enhanchment/models/sale_subscription.py
@@ -22,9 +22,9 @@ class SalesSubscription(models.Model):
             # mins, hours = math.modf(temp.end_hour_use)
             # _logger.info(str(int(hours)) + ' ' + str(int(mins)))
             hours = str(temp.end_hour_use).split('.')[0]
-            mins = str(int(str(temp.end_hour_use).split('.')[1])*60/10)
-            # _logger.info(hours + ':' + mins)
-            _logger.info(temp.end_hour_use)
+            mins = str(int(str(temp.end_hour_use).split('.')[1])*60/10**len(str(temp.end_hour_use).split('.')[1]))
+            _logger.info(hours + ':' + mins)
+            # _logger.info(temp.end_hour_use)
 
 
 
