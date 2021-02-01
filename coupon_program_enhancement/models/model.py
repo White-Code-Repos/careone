c94d2e831c5ba30c23638d945d21deaea99df186
MohabAhmedHamed1999 <mahmed@white-code.co.uk>
2021-02-01 11:11:43 +0000

    test

--- coupon_program_enhancement/models/model.py
+++ coupon_program_enhancement/models/model.py
@@ -2,7 +2,7 @@
 
 from odoo import models, fields, api, _
 from datetime import date
-from odoo.exceptions import UserError, ValidationError
+from odoo.exceptions import ValidationError
 
 class SaleCoupon(models.Model):
 
@@ -11,11 +11,14 @@ class SaleCoupon(models.Model):
     validity_duration = fields.Integer(string="Validity Duration", compute = '_compute_validity_duration', store = True)
 
     @api.model
-    def _compute_validity_duration(self):
-        for this in self:
-            raise ValidationError('The coupon is not applicable by this customer')
-            # if this.validity_duration == 0:
-            this.validity_duration = this.program_id.validity_duration
+    def create(self, vals):
+        rec = super(SaleCoupon, self).create(vals)
+        self.validity_duration = self.program_id.validity_duration
+        return rec
 
-
-    
+    # @api.model
+    # def _compute_validity_duration(self):
+    #     for this in self:
+    #         raise ValidationError('The coupon is not applicable by this customer')
+    #         # if this.validity_duration == 0:
+    #         this.validity_duration = this.program_id.validity_duration
