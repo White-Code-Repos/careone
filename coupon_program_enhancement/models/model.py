51b03a7e3d26944aaaead14201035fd0f31d1e68
MohabAhmedHamed1999 <mahmed@white-code.co.uk>
2021-02-01 11:35:11 +0000

    test

--- coupon_program_enhancement/models/model.py
+++ coupon_program_enhancement/models/model.py
@@ -8,9 +8,11 @@ class SaleCoupon(models.Model):
 
     _inherit = 'sale.coupon'
 
-    validity_duration = fields.Integer(string="Validity Duration", compute ='compute_validity_duration',store = True)
+    validity_duration = fields.Integer(string="Validity Duration")
+    validity_duration1 = fields.Integer(string="Validity Duration", compute='compute_validity_duration')
 
     def compute_validity_duration(self):
         for this in self:
             if this.validity_duration == 0:
+                this.validity_duration1 = this.program_id.validity_duration
                 this.validity_duration = this.program_id.validity_duration
