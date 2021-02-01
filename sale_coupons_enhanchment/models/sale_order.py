396d6c3fc51f3775bcd81d3b2580fa2776e2373b
MohabAhmedHamed1999 <mahmed@white-code.co.uk>
2021-02-01 08:14:38 +0000

    test

--- sale_coupons_enhanchment/models/sale_order.py
+++ sale_coupons_enhanchment/models/sale_order.py
@@ -2,21 +2,20 @@
 from odoo import api, fields, models, _
 
 class AccountMove(models.Model):
-    _inherit = "account.move"
+    _inherit = "account.move.line"
 
     used_coupon = fields.Many2one('sale.coupon', string="Used Coupon", compute='_compute_used_coupon', store=True)
 
     @api.model
     def _compute_used_coupon(self):
         for this in self:
+            pass
             sale_order = this.env['sale.order'].search([('name', '=', this.invoice_origin)])
             if sale_order and sale_order.used_coupon:
                 this.used_coupon = sale_order.used_coupon.id
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
-
-    used_coupon = fields.Many2one('sale.coupon', string="Used Coupon")
 
     def _get_reward_line_values(self, program):
         self.ensure_one()
@@ -26,17 +25,11 @@ class SaleOrder(models.Model):
             return self._get_reward_values_discount(program)
         elif program.reward_type == 'product':
             return [self._get_reward_values_product(program)]
-        # elif program.reward_type == 'product_discount':
-        #     if program.reward_product_id:
-        #         product = self.order_line.filtered(lambda line: program.reward_product_id == line.product_id)
-        #         if product :
-        #             return [self._get_reward_values_product(program)]
-        #     if program.discount_percentage:
-        #         return self._get_reward_values_discount(program)
-
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
+
+    used_coupon = fields.Many2one('sale.coupon', string="Used Coupon")
 
     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
     def _compute_amount(self):
@@ -49,4 +42,3 @@ class SaleOrderLine(models.Model):
                 'price_total': taxes['total_included'],
                 'price_subtotal': taxes['total_excluded'],
             })
-            # if price > 0 else price
