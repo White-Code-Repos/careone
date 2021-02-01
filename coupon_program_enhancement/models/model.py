a021ed686e505419dc52749c7690298fa00f8c2c
MohabAhmedHamed1999 <mahmed@white-code.co.uk>
2021-02-01 10:59:33 +0000

    test

--- coupon_program_enhancement/models/model.py
+++ coupon_program_enhancement/models/model.py
@@ -1,20 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
-from datetime import date
-from odoo.exceptions import UserError, ValidationError
 
 class SaleCoupon(models.Model):
 
     _inherit = 'sale.coupon'
 
-    validity_duration = fields.Integer(string="Validity Duration", compute = '_compute_validity_duration', store = True)
-
-    @api.model
-    def _compute_validity_duration(self):
-        for this in self:
-            # if this.validity_duration == 0:
-            this.validity_duration = this.program_id.validity_duration
-
+    validity_duration = fields.Integer(string="Validity Duration" )
 
     
