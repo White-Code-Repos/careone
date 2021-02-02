04164dc839a3bb7ed45dc6782e3477a4deec681c
MohabAhmedHamed1999 <mahmed@white-code.co.uk>
2021-02-02 10:16:21 +0000

    test

--- sale_coupons_enhanchment/models/sale_order.py
+++ sale_coupons_enhanchment/models/sale_order.py
@@ -2,33 +2,31 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
 
-# class InvoiceUsedCoupon(models.Model):
-#     _name = "invoice.used.coupon"
-#
-#     name = fields.Many2one('sale.coupon', string="Used Coupon")
-#     invoice_id = fields.Many2one('account.move')
-#
-# class AccountMove(models.Model):
-#     _inherit = "account.move"
-#
-#     used_coupon = fields.One2many('invoice.used.coupon', 'invoice_id', string="Used Coupon", compute='_compute_used_coupon' ,store=True)
-#
-#     def _compute_used_coupon(self):
-#         for this in self:
-#             sale = this.env['sale.order'].search([('name', '=', this.invoice_origin)])
-#             lines = this.env['sale.order.line'].search([('order_id', '=', sale.id)])
-#             used_coupon = []
-#             for line in lines:
-#                 used_coupon.append((0, 0, {
-#                     'name': line.used_coupon.id,
-#                     'invoice_id': this.id,
-#                 }))
-#
-#             if used_coupon:
-#                 this.used_coupon = used_coupon
-#             else:
-#                 this.used_coupon = False
-#
+class InvoiceUsedCoupon(models.Model):
+    _name = "invoice.used.coupon"
+
+    name = fields.Many2one('sale.coupon', string="Used Coupon")
+    invoice_id = fields.Many2one('account.move')
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    used_coupon = fields.One2many('invoice.used.coupon', 'invoice_id', string="Used Coupon")
+
+    def _compute_used_coupon(self):
+        for this in self:
+            sale = this.env['sale.order'].search([('name', '=', this.invoice_origin)])
+            lines = this.env['sale.order.line'].search([('order_id', '=', sale.id)])
+            used_coupon = []
+            for line in lines:
+                used_coupon.append((0, 0, {
+                    'name': line.used_coupon,
+                    'invoice_id': this.id,
+                }))
+            if used_coupon:
+                self.used_coupon = used_coupon
+            else:
+                self.used_coupon = False
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
