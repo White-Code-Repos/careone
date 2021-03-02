3090af694eca4dd25842f19435f809bb1fe56e37
Omnya <orahswan@white-code.co.uk>
2021-03-02 12:31:25 +0200

    [MOD]

--- careone_reports/models/product_warranty.py
+++ careone_reports/models/product_warranty.py
@@ -4,6 +4,6 @@ from odoo import fields, models, api, _
 class ProductWarranty(models.Model):
     _inherit = 'product.warranty'
 
-    company_id = fields.Many2one('res.company', 'Company', readonly=True,
+    company_id = fields.Many2one('res.company', 'Company',
                                  default=lambda self: self.env.user.company_id)
     vehicle_id = fields.Many2one('fleet.vehicle')
