5bd4736cb1beaede4a583699c203d38c26b2c419
Mahmoud Salama <mahmoudalee2@gmail.com>
2021-04-20 14:43:36 +0200

    domain journal

--- payment_serial/models/model.py
+++ payment_serial/models/model.py
@@ -20,3 +20,19 @@ class AccountPayment(models.Model):
 
             self.is_online_payment = self.journal_id.is_online_payment
 
+
+
+    @api.onchange('amount')
+    def on_change_amount(self):
+        allowed = self.env['res.user'].search([('id', '=', self.env.user_id)]).allowed_journals.ids
+
+        journal_id_domain = [
+            ('id', 'in', allowed)
+        ]
+        result = {
+            'domain': {
+                'journal_id': journal_id_domain,
+            },
+        }
+        return result
+    # journal_id = fields.Many2one('account.journal', string='Journal', required=True, readonly=True, states={'draft': [('readonly', False)]}, tracking=True, domain="[('type', 'in', ('bank', 'cash')), ('company_id', '=', company_id)]")
